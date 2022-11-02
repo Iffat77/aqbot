ee1cbc8aaaaeea23d2b6642ea064ced24efe3277
Iffat Hossain <iffathossain77@gmail.com>
2022-11-01 22:08:12 -0400

    alt to prev setup

--- venv/main.py
+++ venv/main.py
@@ -1,23 +1,33 @@
-import tweepy
+import tweepy as twitter
 import keys
 
 # print('this is my twitter bot')
 
-def api():
-  auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
-  auth.set_access_token(keys.access_token, keys.access_token_secret)
 
-  return tweepy.API(auth)
+# def api():
+auth = twitter.OAuthHandler(keys.api_key, keys.api_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
+api = twitter.API(auth)
+    # return tweepy.API(auth)
 
 
-def tweet(api: tweepy.API, message: str, image_path=None):
-  if image_path:
-    api.update_status_with_media(message, image_path)
-  else:
-    api.update_status(message)
-print('Tweeted successfully')
+# def tweet(api: tweepy.API, message: str, image_path=None):
+#     if image_path:
+#         api.update_status_with_media(message, image_path)
+#     else:
+#         api.update_status(message)
 
-if __name__ == '__main__':
-    api = api()
-    tweet(api, 'Testing testing ')
 
+# print('Tweeted successfully')
+
+# if __name__ == '__main__':
+#     api = api()
+#     tweet(api, 'Testing testing ')
+
+
+
+def tweet(text):
+  api.update_status(text)
+  print('Tweeted successfully')
+
+tweet("Hello this is a test tweet")
