366e7c9cecd03af73b415e874f1cd4fa18fde8e3
Iffat Hossain <iffathossain77@gmail.com>
2022-11-04 00:51:16 -0400

    adds res to tweets and sort data

--- venv/main.py
+++ venv/main.py
@@ -1,15 +1,17 @@
 import tweepy as twitter
+import requests 
 import keys
+import re
 
-# print('this is my twitter bot')
+# response_API = requests.get('http://api.waqi.info/feed/newyork/?token=')
+# print(response_API.status_code)
 
+# data = response_API.text
+# parse_json = json.loads(data)
 
-# def api():
-auth = twitter.OAuthHandler(keys.api_key, keys.api_secret)
-auth.set_access_token(keys.access_token, keys.access_token_secret)
-api = twitter.API(auth)
-    # return tweepy.API(auth)
+# print(parse_json)
 
+# --------------------------
 
 # def tweet(api: tweepy.API, message: str, image_path=None):
 #     if image_path:
@@ -24,10 +26,68 @@ api = twitter.API(auth)
 #     api = api()
 #     tweet(api, 'Testing testing ')
 
+# -------------------------
+
+auth = twitter.OAuthHandler(keys.api_key, keys.api_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
+api = twitter.API(auth)
+
+FILE = "id.txt"
+
+def retrieve_id(file):
+  f_read=open(file, "r")
+  last_seen_id = int(f_read.read().strip())
+  f_read.close()
+  return last_seen_id
+
+def store_id(id, file):
+  f_write = open(file, "w")
+  f_write.write(str(id))
+  f_write.close()
+  return
+
+last_seen_id = retrieve_id(FILE)
+mentions = api.mentions_timeline(last_seen_id, tweet_mode="extended")
+
+# mentions = api.mentions_timeline()
+# for mention in mentions:
+#     tweet = (mention.text)
+#     print(tweet, "printing tweet")
+#     clean_tweet = re.sub("@[A-Za-z0-9_]+","", tweet)
+#     clean_tweet = re.sub("#[A-Za-z0-9_]+","", clean_tweet)
+#     clean_tweet = clean_tweet.lower()
+#     clean_tweet = clean_tweet.replace(" ", "")
+#     print(clean_tweet, "this is the clean tweet")
+
+for mention in reversed(mentions):
+    if "airqual" in mention.full_text:
+      tweet = (mention.full_text)
+      # print(tweet, "printing tweet")
+      clean_tweet = re.sub("@[A-Za-z0-9_]+","", tweet)
+      # print(clean_tweet, "this is clean tweet no @")
+      clean_tweet = re.sub("#[A-Za-z0-9_]+","", clean_tweet)
+      # print(clean_tweet, "clean tweet no @ or #")
+      clean_tweet = clean_tweet.lower()
+      clean_tweet = clean_tweet.replace(" ", "")
+      # print(clean_tweet, "this is the clean tweet")
+      # logic to sort location for api search params go here
+      last_seen_id = mention.id
+      store_id(last_seen_id, FILE)
+      api.update_status('@'+mention.user.screen_name + ' '+ clean_tweet + ' we are currently testing ' , mention.id)
+      print('Replied to @ ' + mention.user.screen_name)
 
 
-def tweet(text):
-  api.update_status(text)
-  print('Tweeted successfully')
 
-tweet("Hello this is a test tweet")
+
+# def tweet(text):
+#   api.update_status(text)
+#   print('Tweeted successfully')
+
+# tweet("Hello this is a test tweet")
+
+
+'''
+import requests
+from requests_oauthlib import OAuth1
+import os
+'''
