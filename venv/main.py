e19bb7f5a02e490323ee13307d0bec833554b013
Iffat Hossain <iffathossain77@gmail.com>
2022-11-08 10:42:34 -0500

    blockers resolved, issues in regard to white space caused errors

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,6 @@
 import tweepy as twitter
 import requests 
 import asyncio
-import aiohttp
 import time
 import json
 import keys
@@ -19,35 +18,22 @@ import re
 # asyncio.run(main())
 
 city = ''
-results = []
-aqual = 0
+aqual = None
 
-
-# async def get_data():
-#   async with aiohttp.ClientSession() as session:
-#     new_data = None
-#     response = await session.get(f'http://api.waqi.info/feed/{city}/?token={keys.API_TOKEN}', ssl=False)
-#     results.append(await response.json())
-#     result = results[0]
-#     for key in result.keys():
-#       if key == 'data':
-#         new_data = (key, result[key])
-#         aqual = new_data[1]['aqi']
-#         print(aqual)
-#         return aqual
-
-def get_data(city):
-    new_data = None
-    response = requests.get(f'http://api.waqi.info/feed/{city}/?token={keys.API_TOKEN}')
-    data = response.text
-    parse_json = json.loads(data)
-    for key in parse_json.keys():
-      if key == 'data':
-        new_data = (key, parse_json[key])
-        aqual = new_data[1]['aqi']
-        print(aqual)    
+def get_data():
+  new_data = None
+  response = requests.get(f'http://api.waqi.info/feed/{city}/?token={keys.API_TOKEN}')
+  data = response.text
+  parse_json = json.loads(data)
+  for key in parse_json.keys():
+    if key == 'data':
+      new_data = (key, parse_json[key])
+      aqual = new_data[1]['aqi']
+      print(aqual) 
+      return aqual   
     
-        
+
+
 
 auth = twitter.OAuthHandler(keys.api_key, keys.api_secret)
 auth.set_access_token(keys.access_token, keys.access_token_secret)
@@ -67,11 +53,15 @@ def store_id(id, file):
   f_write.close()
   return
 
+def reply():
+  api.update_status((f'@{mention.user.screen_name} Air Quality Index in {city} is currently {aqual} '), mention.id)
+      # api.update_status('@'+mention.user.screen_name + ' '+ city + ' air quality is currently '+ aqual ' ' , mention.id)
+  print('Replied to @ ' + mention.user.screen_name)
+
 last_seen_id = retrieve_id(FILE)
 mentions = api.mentions_timeline(last_seen_id, tweet_mode="extended")
 
-def main(aqual):
-  for mention in reversed(mentions):
+for mention in reversed(mentions):
     if "airqual" in mention.full_text:
       tweet = (mention.full_text)
       # print(tweet, "printing tweet")
@@ -79,31 +69,24 @@ def main(aqual):
       # print(clean_tweet, "this is clean tweet no @")
       clean_tweet = re.sub("#[A-Za-z0-9_]+","", clean_tweet)
       # print(clean_tweet, "clean tweet no @ or #")
+      clean_tweet = re.sub("''[A-Za-z0-9_]+","", clean_tweet)
       clean_tweet = clean_tweet.lower()
       clean_tweet = clean_tweet.replace(" ", "")
+      clean_tweet = " ".join(clean_tweet.split())
       # print(clean_tweet, "this is the clean tweet")
       # logic to sort location for api search params go here
-      city = clean_tweet 
-      get_data(city)
+      city = clean_tweet
+      print(city)
+      aqual = get_data()
+      print(aqual, 'here')
       last_seen_id = mention.id
       store_id(last_seen_id, FILE)
-      api.update_status((f'@{mention.user.screen_name} {aqual} we are currently testing '), mention.id)
-      # api.update_status('@'+mention.user.screen_name + ' '+ city + ' air quality is currently '+ aqual ' ' , mention.id)
-      print('Replied to @ ' + mention.user.screen_name)
-
-if aqual != 0:
-  main(aqual)
-else:
-  get_data(city)
-
-
+      reply()
+      
 
 # get_data function 
 # reply function
 # make for loop for mentions a function
-
-
-
 '''
 import requests
 from requests_oauthlib import OAuth1
