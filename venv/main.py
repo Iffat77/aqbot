87b946c2180228660a8d20aa47ec2acae9580ca3
Iffat Hossain <iffathossain77@gmail.com>
2022-11-21 13:27:48 -0500

    resolves save id issue, needs fix for reply

--- venv/main.py
+++ venv/main.py
@@ -9,6 +9,7 @@ import re
 city = ''
 aqual = None
 error_msg = None
+
 while True:
   def get_data():
     new_data = None
@@ -16,7 +17,6 @@ while True:
         f'http://api.waqi.info/feed/{city}/?token={keys.API_TOKEN}')
     data = response.text
     parse_json = json.loads(data)
-    # print(parse_json['status'], 'here')
     if parse_json['status'] == 'error':
       err_reply()
     elif parse_json['status'] == 'ok':
@@ -32,14 +32,7 @@ while True:
   api = tweepy.API(auth)
 
   client_id = client.get_me().data.id
-  start_id = 1
-  message = ''
-
-  def reply():
-    start_id = twee_id
-    message = (f'PM2.5 Air Quality Index in {city} is currently {aqual}') 
-    print(message)
-    print(start_id, 'start id ')
+  start_id = None
   
   def err_reply():
     print('there was an error')
@@ -49,14 +42,11 @@ while True:
 
 
   res = client.get_users_mentions(client_id, since_id=start_id)
-  # print(res.data[0])
 
   if res.data != None:
-    # mention = str(res.data[0])
     for twee in res.data:
       print(start_id, 'first id')
       twee_id = res.meta['newest_id']
-      # print(twee_id, 'this the id')
       mention = (str(twee))
       if 'airqual' in mention:
         tweet = mention
@@ -67,54 +57,20 @@ while True:
         clean_tweet = clean_tweet.replace(" ", "")
         clean_tweet = " ".join(clean_tweet.split())
         city = clean_tweet
-        # print(city)
         aqual = get_data()
-        if aqual != None:
-          print(f'{aqual} for {city}')
-      try:
-        print(city, 'here')
-        reply()
-        client.create_tweet(in_reply_to_tweet_id=twee_id, text=message)
-        # print(twee_id)
-        # start_id = twee_id
-        # print(start_id, 'second id')
-      except:
-        pass
-  # if "airqual" in mention:
-  #   print('airqual in mention')
-  #   tweet = mention
-  #   clean_tweet = re.sub("@[A-Za-z0-9_]+", "", tweet)
-  #   clean_tweet = re.sub("#[A-Za-z0-9_]+", "", clean_tweet)
-  #   clean_tweet = re.sub("''[A-Za-z0-9_]+", "", clean_tweet)
-  #   clean_tweet = clean_tweet.lower()
-  #   clean_tweet = clean_tweet.replace(" ", "")
-  #   clean_tweet = " ".join(clean_tweet.split())
-  #   city = clean_tweet
-  #   print(city)
-  #   aqual = get_data()
-  #   if aqual != None:
-  #     print(f'{aqual} for {city}')
-      # try:
-      #   print(mention.text)
-      #   client.create_tweet(in_reply_to_tweet_id=mention.id, text=reply())
-      # except:
-      #   pass
-
-
-  # def reply():
-  #   print(aqual)
-    # api.update_status(
-    #     (f'@{mention.user.screen_name} PM2.5 Air Quality Index in {city} is currently {aqual} '), mention.id)
-    # print('Replied to @ ' + mention.user.screen_name)
-
-
-  # last_seen_id = retrieve_id(FILE)
-  # mentions = api.mentions_timeline(last_seen_id, tweet_mode="extended")
-
-        
+      if aqual != None:
+        message = (f'PM2.5 Air Quality Index in {city} is currently {aqual}') 
+        try:
+          print(city, 'here')
+          print(message)
+          client.create_tweet(in_reply_to_tweet_id=twee_id, text=message)
+          start_id = twee_id
+          print(start_id, 'new id')
+        except Exception as error:
+          print(error)
 
   print("sleeping")
-  time.sleep(5)
+  time.sleep(30)
   print('awake')
 
-# figure out why start id is not updating 
+
